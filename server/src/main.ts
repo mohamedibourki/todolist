b181374edc22947acddd41b55012e162c6385739
Mohamed ibourki <mohamedibourkioffice@gmail.com>
2025-08-05 23:10:57 +0100

    update client url

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://todolist-vert-sigma.vercel.app',
   });
 
   await app.listen(8001, '0.0.0.0');
